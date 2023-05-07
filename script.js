055e51819cb590993030a3947a0f567993aa2483
Raghavan <raghavan.work@outlook.com>
2023-05-07 19:18:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//1,2,3,4,5,6,7,8,9,0,'*','/','+','='
+var num=[];
+var name2;
+var iq;
+var a=0;
+function numl(i){
+  iq=i; 
+  if( a<i.length);{
+   num[a]=i;
+  }
+
+  
+    document.getElementById("output").innerHTML=num[a];
+}
+for(q=0;q<iq.length;q++)
+{
+
+  console.log(num[q]);
+}
+
+function arfun(s){
+    console.log(s);
+
+
+}
